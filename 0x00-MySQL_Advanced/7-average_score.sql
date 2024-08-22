27257fb558143d3f8f9aa84a5ea3f37eb83d9382
Solotole <solomontoleak47@gmail.com>
2024-08-22 17:44:27 +0100

    CREAT PROCEDURE for automatically calculating user scores average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,27 +5,26 @@ CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE count INT DEFAULT 0;
     DECLARE total INT DEFAULT 0;
-    -- DECLARE users_id INT DEFAULT 0;
-
-    -- SET users_id = user_id
+    DECLARE average FLOAT DEFAULT 0;
     -- number of projects of a user
     SELECT COUNT(*) INTO count
     FROM corrections
     WHERE user_id = user_id;
     -- total of scores of a student
-    SELECT SUM(score) INTO total
+    SELECT COALESCE(SUM(score), 0) INTO total
     FROM corrections
     WHERE user_id = user_id;
     -- updating new average score of the user
+
     IF count > 0 THEN
-        UPDATE users
-        SET average_score = total / count
-        WHERE id = user_id;
+        SET average = total / count;
     ELSE
-        UPDATE users
-        SET average_score = 0
-        WHERE id = user_id;
+        SET average = 0;
     END IF;
+
+    UPDATE users
+    SET average_score = average
+    WHERE id = user_id;
 END //
 
 DELIMITER ;
