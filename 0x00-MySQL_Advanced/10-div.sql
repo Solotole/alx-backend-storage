53e8a443a71163c7c5cfe146e00fbf4f2097fe1b
Solotole <solomontoleak47@gmail.com>
2024-08-23 02:33:40 +0100

    creating a function

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,9 +2,8 @@
 -- argument and returns result. if second equals 0 then return 0
 DELIMITER //
 
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURN FLOAT
-DETERMINISTIC
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURN FLOAT DETERMINISTIC
 BEGIN
     RETURN (IF (b = 0, 0, a / b));
 END //
