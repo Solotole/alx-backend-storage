43b19abd66648f95a0de3971d5c1106d1090261e
Solotole <solomontoleak47@gmail.com>
2024-08-23 02:32:00 +0100

    creating a function

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -6,11 +6,7 @@ CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURN FLOAT
 DETERMINISTIC
 BEGIN
-    IF b = 0 THEN
-        RETURN 0;
-    ELSE
-        RETURN a / b;
-    END IF; 
+    RETURN (IF (b = 0, 0, a / b));
 END //
 
 DELIMITER ;
