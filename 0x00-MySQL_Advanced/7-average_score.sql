711a8e232e8c485b7177ca529c0e97ffa2085e95
Solotole <solomontoleak47@gmail.com>
2024-08-23 02:09:33 +0100

    AVG score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,30 +1,13 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student
 DELIMITER //
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE count INT DEFAULT 0;
-    DECLARE total INT DEFAULT 0;
-    DECLARE average FLOAT DEFAULT 0;
-    -- number of projects of a user
-    SELECT COUNT(*) INTO count
-    FROM corrections
-    WHERE user_id = user_id;
-    -- total of scores of a student
-    SELECT COALESCE(SUM(score), 0) INTO total
-    FROM corrections
-    WHERE user_id = user_id;
-    -- updating new average score of the user
-
-    IF count > 0 THEN
-        SET average = total / count;
-    ELSE
-        SET average = 0;
-    END IF;
-
     UPDATE users
-    SET average_score = average
-    WHERE id = user_id;
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
 END //
 
 DELIMITER ;
