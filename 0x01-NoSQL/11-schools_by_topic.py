16b5065943455a4777cc7d69c279071f2e03219e
Solotole <solomontoleak47@gmail.com>
2024-08-27 16:02:22 +0100

    finding by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" finding documents with specific topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ function that returns the list of school
+        having a specific topic
+    """
+    list_topics = []
+
+    list_topics.extend(mongo_collection.find({'topics' : topic}))
+    return list_topics
