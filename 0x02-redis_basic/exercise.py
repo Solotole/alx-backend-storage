af12d4f0db242724e9950395fcedb71d63f6e9b6
Solotole <solomontoleak47@gmail.com>
2024-08-30 15:35:49 +0100

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -42,35 +42,6 @@ class Cache:
             return result
         return wrapper
 
-    def cache_page(method: Callable) -> Callable:
-        """
-        Decorator to cache the result of the `get_page` function and
-        count the number of times a URL is accessed.
-        Args:
-            method (Callable): The function to be decorated.
-        Returns:
-            Callable: A wrapper function that caches the result and
-            tracks the access count.
-        """
-        @wraps(method)
-        def wrapper(url: str):
-            """
-            Wrapper function that implements caching and access counting.
-            Args:
-                url (str): The URL to fetch the HTML content from.
-            Returns:
-                str: The HTML content of the URL, either from the cache
-                or by making a new HTTP request.
-            """
-            self._redis.incr(f"count:{url}")
-            cached_content = self._redis.get(f"cached:{url}")
-            if cached_content:
-                return cached_content.decode('utf-8')
-            content = method(url)
-            self._redis.setex(f"cached:{url}", 10, content)
-            return content
-        return wrapper
-
     def call_history(method: Callable) -> Callable:
         """
         Decorator to store the history of inputs and outputs
@@ -92,21 +63,6 @@ class Cache:
 
             return output
         return wrapper
-
-    @cache_page
-    def get_page(url: str) -> str:
-        """
-        Fetches the HTML content of a specified URL.
-        This function makes an HTTP GET request to the provided URL and
-        returns the HTML content. If decorated with `cache_page`, it also
-        caches the content in Redis and tracks the number of accesses.
-        Args:
-            url (str): The URL to fetch the HTML content from.
-        Returns:
-            str: The HTML content of the URL.
-        """
-        response = requests.get(url)
-        return response.text
 
     @call_history
     @count_calls
